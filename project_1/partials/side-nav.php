9242a9a40ab1f94c08741a12b6e211b25a1d5f40
KelseyF11 <35752484+KelseyF11@users.noreply.github.com>
2018-03-01 18:42:12 -0800

    Files uploaded to GitHub

---
+++ project_1/partials/side-nav.php
@@ -0,0 +1,21 @@
+	<header>
+		<h1><a href="index.php">HTML & CSS Component Library</a></h1>
+			<div>
+				<ul class="navlist">
+					<li class="navlist-item"><a class="navlist-link" href="alerts.php">Alerts</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="badges.php">Badges</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="breadcrumbs.php">Breadcrumbs</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="buttons.php">Buttons</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="cards.php">Cards</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="footer.php">Footers</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="forms.php">Forms</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="global-navigation.php">Global Navigation</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="#">Hero Units</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="#">Lists</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="#">Media Objects</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="#">Navigation Lists</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="#">Pagination</a></li>
+					<li class="navlist-item"><a class="navlist-link" href="#">Tables</a></li>
+				</ul>
+			</div>
+	</header>
